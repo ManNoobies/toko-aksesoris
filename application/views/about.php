90afcd2eeeb5a23afc8e608277467702871cd6cc
ManNoobies <145945501+ManNoobies@users.noreply.github.com>
2023-11-30 18:36:34 +0700

    Add files via upload

---
+++ application/views/about.php
@@ -0,0 +1,51 @@
+
+<h2 class="text-center">About Us</h2>
+<div class="col-12">
+    <br> 
+    <p class="text-center">D
+Here we created an E-Commerce website selling accessories with the website name, namely E-Commerce, we have Home, Contact Us, About Us, Purchase Cart
+.<br>(only visible to members).</p>
+    <p class="text-center">We have 3 categories of accessories that we sell, namely Watches, Rings, Necklaces. Where everyone can see our website, here visitors can see our website, but they cannot buy it because they need to register as members in our shop </p>
+</div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="aboutus-banner">
+                        <img src="http://themeinnovation.com/demo2/html/build-up/img/home1/about1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-5 col-sm-6 col-xs-12">
+                    <div class="feature">
+                        <div class="feature-box">
+                            <div class="clearfix">
+                                <div class="iconset">
+                                    <span class="glyphicon glyphicon-cog icon"></span>
+                                </div>
+                        
+                            </div>
+                        </div>
+                        <div class="feature-box">
+                            <div class="clearfix">
+                                <div class="iconset">
+                                    <span class="glyphicon glyphicon-cog icon"></span>
+                                </div>
+                            
+                            </div>
+                        </div>
+                        <div class="feature-box">
+                            <div class="clearfix">
+                                <div class="iconset">
+                                    <span class="glyphicon glyphicon-cog icon"></span>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
