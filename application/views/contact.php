dc4ebd19c1273460e5bbb804f470174522adc8c4
ManNoobies <145945501+ManNoobies@users.noreply.github.com>
2023-11-30 18:37:17 +0700

    Add files via upload

---
+++ application/views/contact.php
@@ -0,0 +1,27 @@
+<div class="row" style="height:550px;">
+    <div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6111681242683!2d106.8401040745306!3d-6.18276486057937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f449c647e8ab%3A0x49341f96265c01f8!2sJl.%20Kramat%20Raya%20No.98%2C%20RT.2%2FRW.9%2C%20Kwitang%2C%20Kec.%20Senen%2C%20Kota%20Jakarta%20Pusat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2010420!5e0!3m2!1sen!2sid!4v1686100020198!5m2!1sen!2sid" width="600" height="450" style="border:0;margin-top:50px;margin-left:30px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class="col-md-6">
+      <h2 class="text-uppercase mt-3 font-weight-bold text-black">Having Trouble?</h2>
+      <h3>Feel free to contact us!</h3>
+        <div class="details">
+          <p>Get in touch!</p>
+          <h4>Head Office</h4>
+            <div>
+              <li>
+                <span class="oi oi-map"></span> 
+                <p>Jl. Kramat Raya No.98, RW.9, Kwitang</p>
+              </li>
+              <li>
+                <span class="oi oi-phone"></span><br>
+                <p>408-317-3645</p>
+              </li>
+              <li>
+                <span class="oi oi-clock"></span><br>
+                <p>10.00 - 20.00, Mon - Fri</p>
+              </li>
+            </div>
+        </div>
+    </div>
+</div>
